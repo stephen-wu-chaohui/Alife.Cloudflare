6dc441fe153410668387046947eb4098aa784a7d
Stephen Wu <stephen.wu.chaohui@gmail.com>
2026-04-18 08:49:24 +1200

    Use https://images.ccalc.live for images API

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://soft-wood-459e.app-ccalc.workers.dev',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env vars regardless of the VITE_ prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          // Use the variable loaded from your .env file
+          target: env.R2_PUBLIC_BASE_URL || 'https://images.ccalc.live',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
